04bb8ea6070787dbca18e66c9a240196c75709f7
simo nuorala <simonu@metropolia.fi>
2026-09-21 19:07:40 +0300

    peliprojekti tehtävä 3.

--- peliprojekti/peliprokk.py
+++ peliprojekti/peliprokk.py
@@ -1,3 +1,54 @@
+class esine:
+    #Yksittäinen inventaarioon lisättävä esine.
+
+    def __init__(self, nimi):
+        self.nimi = nimi
+        
+
+    def kayta(self):
+        #Käytä esinettä ja palauta käyttäjälle ilmoitettava viesti.
+        return f'Käytit esinettä: {self.nimi}'
+
+    def tiedot(self):
+        #Palauta esineen nimi
+        if self.kuvaus:
+            return f'{self.nimi}'
+        return self.nimi
+
+    def __str__(self):
+        return self.tiedot()
+
+
+#tyhjä lista esineille
+inventory = []
+
+def tervehdi(pnimi):
+    print(f'terve {pnimi}')
+
+def lisaa_esine():
+    nimi = input("lisää esine inventaarioon: ")
+    uusi_esine = esine(nimi)
+    inventory.append(uusi_esine)
+    print(f'lisätty: {uusi_esine}')
+
+def nayta_inventaario():
+    print('\n-- inventaarion sisältö-- ')
+    if not inventory:
+        print("inventaario on tyhjä")
+    else:
+        x = 1
+        for esine in inventory:
+            print(f'{x}. {esine}')
+            x += 1
+    print('-------------------------------------------')
+
+def lebron_highlights():
+    print('lebron highlights joskus!')
+
+
+
+
+#pääohjelma
 name = input("anna nimesi: ")
 age = int(input("kuinka vanha olet:"))
 print("hei", name)
